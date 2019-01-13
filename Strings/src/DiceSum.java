14762302437a6dd81849bb48ad071245d0e5c33c
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-13 20:36:07 +0530

    Dice Roll Sum Problem

--- Strings/src/DiceSum.java
+++ Strings/src/DiceSum.java
@@ -1,34 +1,38 @@
 import java.util.*;
-class DiceRoll{
+class DiceSum{
     public static void main (String[] args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of dice: ");
         int n = sc.nextInt();
-        System.out.println("Possible outcomes are:  ");
-        DiceRoll(n);
+        System.out.println("Enter the total sum desired:");
+        System.out.println("Possible permutations giving the desired sum : ");
+        int sum_desired = sc.nextInt();
+        DiceSum(n,sum_desired);
     }
-    public static void DiceRoll(int n)
+    public static void DiceSum(int n, int sum_desired)
     {
         ArrayList<Integer> res = new ArrayList<Integer>();
-        DiceRollHelper(n,res);
+        DiceSumHelper(n,res,sum_desired);
     }
-    public static void DiceRollHelper(int n,ArrayList<Integer> res)
+    public static void DiceSumHelper(int n,ArrayList<Integer> res, int sum_desired)
     {
         if(n==0)
-        {
+        {   if(sum_desired==0){
             System.out.print("( ");
             for(int i= 0; i<res.size();i++)
             System.out.print(res.get(i)+" ");
             System.out.println(")");
         }
+        }
         else
+        {   if(!((n*6<sum_desired)||(n>sum_desired)))
         {
             for(int i = 1 ; i<=6; i++)
             {res.add(i);
-             DiceRollHelper(n-1,res);
+             DiceSumHelper(n-1,res,sum_desired-i);
              res.remove(res.size()-1);   }
-
+        }
         }
     }
 }
