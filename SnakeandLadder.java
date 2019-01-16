191c2eec16df0e32eb7a686a6da2e89175270ff1
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-16 21:45:35 +0530

    .

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,23 +1,62 @@
 class SnakeandLadder 
 {
+
+// Given a history of dice outcome, print if the player reaches board end or not
     public static void main(String[] args)
     {
-        int n = 30;
-        int[] board = new int[n];
-        for(int i = 0; i<n; i++)
-        //ladder
-        board[i] = -1;
-        board[2] = 21;
-        board[4] = 7;
-        board[10] = 25;
-        board[19] = 28;
-        //Snake
-        board[26] = 0;
-        board[20] = 8;
-        board[16] = 3;
-        board[18] = 6;
-
-        System.out.println("Minimum number of controlled dice throws are: "+ MinimumDiceThrows(boards, n));
+        int[] snl = new int[21]; // snake and ladder of size 20
+        snl[3] = 17;
+        snl[7] = 11;
+        snl[13] = 5;
+        snl[19] = 2;
+        int[] moves1 = {2, 5, 3, 4,6, 3, 4, 3, 5, 1, 2, 3};
+        int[] moves2 = {2, 5, 3, 4,6, 3, 4, 3, 5, 1, 1, 6, 5, 2, 3, 5};
+        PrintSnL(0,20,snl,moves1,0);
+        PrintSnL(0,20,snl,moves2,0);
     }
-    
+    public static void PrintSnL(int src, int des, int[] snl, int[] moves, int mvidx) {
+        if(src == des)
+        {
+            System.out.println("You Win!!");
+            return;
+        }
+        if(mvidx == moves.length)
+        {
+            System.out.println(src);
+            return;
+        }
+        if(src == 0)
+        {
+            if (moves[mvidx] == 1 || moves[mvidx] == 6)
+            {
+                src = 1;
+                PrintSnL(src,des,snl,moves,mvidx+1);
+            }
+            else
+            {
+                PrintSnL(src,des,snl,moves,mvidx+1);
+            }
+
+        }
+        else
+        {
+            if(snl[src]!=0)
+            {
+                src = snl[src];
+                PrintSnL(src,des,snl,moves,mvidx);
+            }
+            else
+            {
+                src = src + moves[mvidx];
+                if(src<=des)
+                PrintSnL(src,des,snl,moves,mvidx+1);
+                else
+                PrintSnL(src-moves[mvidx],des,snl,moves,mvidx+1);
+            }
+        }
+        
+        
+    }
+
+
 }
