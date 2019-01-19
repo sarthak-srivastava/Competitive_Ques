c235b2e30e5cab3dfdc6c31362b7b8a1f6f629dd
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-19 21:06:09 +0530

    Problem Solved using String builder. We Need to undo the affect after each call in order to traverse other possible branches of decision tree correctly

--- StringPermute.java
+++ StringPermute.java
@@ -3,7 +3,10 @@ class StringPermute{
     public static void main(String[] args)
     {
         String ques = "abc";
+        StringBuilder a = new StringBuilder("abc");
         printPermute(ques,"");
+        System.out.println();
+        printPermute(a,new StringBuilder());
 
     }
     public static void printPermute(String ques, String asf)
@@ -20,4 +23,23 @@ class StringPermute{
             printPermute(roq, asf+ch); 
     }
 }
+    public static void printPermute(StringBuilder ques, StringBuilder asf)
+    {
+        if(ques.length()==0)
+        {
+            System.out.print(asf +" ");
+            return;
+        }
+        else
+        {
+            for(int i = 0; i<ques.length();i++)
+            {
+                char ch = ques.charAt(i);
+                ques.deleteCharAt(i);
+                printPermute(ques,asf.append(ch));
+                asf.deleteCharAt(asf.length()-1);
+                ques.insert(i,ch);      // We Need to undo the affect after each call to backtrack to another branch of possible tree.
+            }
+        }
+    }
 }
