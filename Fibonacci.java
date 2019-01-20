bbfe7ee8e997610fe9fe0a16f4d0452817d2d310
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-20 23:56:23 +0530

    Tabloid method

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,6 +6,7 @@ class Fibonacci{
         int n = sc.nextInt();
         long[] qb = new long[n+1];
         System.out.println(Fibm(n - 1,qb));
+        System.out.println(Fibt(n-1));
     }
     public static long Fibm(int n, long[] qb)
     {
@@ -17,5 +18,13 @@ class Fibonacci{
         qb[n] = ans;
         return qb[n];
     }
-
+    public static int Fibt(int n)
+    {
+        int[] f = new int[n+1];
+        f[0] = 0;
+        f[1] = 1;
+        for(int i = 2;i<=n;i++)
+        f[i] = f[i-1]+f[i-2];
+        return f[n];
+    }
 }
