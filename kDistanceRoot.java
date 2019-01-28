2414f5eb4839bd5290513599975a877a59ac57db
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-28 20:41:07 +0530

    https://practice.geeksforgeeks.org/problems/k-distance-from-root/1

---
+++ kDistanceRoot.java
@@ -0,0 +1,98 @@
+{
+// Java program to find maximum sum leaf to root
+// path in Binary Tree
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+// A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+class Main
+{
+    // driver function to test the above functions
+    public static void main(String args[])
+    {
+        // Input the number of test cases you want to run
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+            HashMap<Integer, Node> m = new HashMap<Integer, Node> ();
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            Node root = null;
+            while (n > 0)
+            {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+                
+                //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+                Node parent = m.get(n1);
+                if (parent == null)
+                {
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+                n--;
+            }
+            GfG g = new GfG();
+            g.printKdistance(root, k);
+            System.out.print("
+");
+            t--;
+        }
+    }
+}
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+class GfG
+{
+     // Recursive function to print right view of a binary tree.
+     void printKdistance(Node root, int k)
+     {
+          // Your code here
+          printHelper(root,k,0);
+     }
+     void printHelper(Node root, int k, int level)
+     {
+         if(level == k && root!=null)
+         System.out.print(root.data+" ");
+         if(level>k)
+         return;
+         if(root!=null)
+         {
+         printHelper(root.left,k,level+1);
+         printHelper(root.right,k,level+1);}
+     }
+}
