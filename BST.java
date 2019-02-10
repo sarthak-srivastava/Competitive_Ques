d860310a55819ecb41e14f1a8cd03dea3b6be4be
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-10 23:49:54 +0530

    Create BST.java

---
+++ BST.java
@@ -0,0 +1,32 @@
+ public class BinarySearchTreeNode{
+ private int data;
+ private BinarySearchTreeNode left;
+ private BinarySearchTreeNode right;
+ public int getData()
+ {
+ return this.data;
+ }
+ public void setData(int data)
+ {
+  this.data = data; 
+ }
+ public BinarySearchTreeNode getLeft()
+ {
+  return left;
+ }
+ 
+ public void setLeft(BinarySearchTreeNode left)
+ {
+  this.left = left;
+ }
+ 
+ public BinarySearchTreeNode getRight()
+ {
+  return right;
+ }
+ public void setRight(BinarySearchTreeNode right)
+ {
+  this.right = right;
+ }
+ 
+ }
