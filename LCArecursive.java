8e5080cff13c436dbe69167f5a44718fb001cb52
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-13 15:33:21 +0530

    https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/

---
+++ LCArecursive.java
@@ -0,0 +1,93 @@
+
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+class Lowest_Common_Ancestor
+{
+    // driver function to test the above functions
+    public static void main(String args[])
+    {
+        // Input the number of test cases you want to run
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+            HashMap<Integer, Node> m = new HashMap<Integer, Node> ();
+            int n = sc.nextInt();
+            Node root = null;
+            while (n > 0)
+            {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+                //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+                Node parent = m.get(n1);
+                if (parent == null)
+                {
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+                n--;
+            }
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+            GfG g = new GfG();
+            Node k = g.LCA(root,a,b);
+			System.out.println(k.data);
+			//System.out.println();
+            t--;
+			
+        }
+    }
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class GfG
+{
+	Node LCA(Node root, int n1,int n2)
+	{
+		// Your code here
+		if(root == null)
+		return null;
+		if(root.data == n1 || root.data == n2)
+		return root;
+		Node left_lca = LCA(root.left,n1,n2);
+		Node right_lca = LCA(root.right,n1,n2);
+		if(left_lca!=null && right_lca!=null)
+		return root;
+		if(left_lca==null && right_lca == null)
+		return null;
+		return left_lca!=null?left_lca:right_lca;
+	}
+}
