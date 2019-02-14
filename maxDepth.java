dcce95c25ca2debfbf956bc60fd783f851668125
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-14 22:29:29 +0530

    Create maxDepth.java

---
+++ maxDepth.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        else
+            return 1+Integer.max(maxDepth(root.left),maxDepth(root.right));
+        
+    }
+}
