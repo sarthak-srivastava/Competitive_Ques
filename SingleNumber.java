c4fcec174bf38093b53eb6af9fbd59e64ac23bc6
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-18 10:46:45 +0530

    https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/549/

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,9 +1,8 @@
-public class Solution {
-    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
-    public int singleNumber(final int[] A) {
-        int num = 0;
+class Solution {
+    public int singleNumber(int[] A) {
+        int num = A[0];
         for(int i =1; i<A.length;i++)
-        {num = A[i]^A[i-1];}
+        {num = A[i]^num;}
         return num;
     }
 }
