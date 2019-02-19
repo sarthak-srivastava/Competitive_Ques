2ccd58570e6bcf22dc498c7d8977eae7a15a2584
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-19 22:26:04 +0530

    Update dp1.java

--- DP/dp/src/dp/dp1.java
+++ DP/dp/src/dp/dp1.java
@@ -12,11 +12,16 @@ public class dp1 {
 		System.out.println(Fibm(n, new int[n + 1]));
 		end = System.currentTimeMillis();
 		System.out.println(end - start);
+		
+		start = System.currentTimeMillis();
+		System.out.println(Fibt(n));
+		end = System.currentTimeMillis();
+		System.out.println(end - start);
 	}
 
 	public static int Fib(int n) {
 		if (n == 0 || n == 1)
-			return n;
+			return 1;
 		else
 			return Fib(n - 1) + Fib(n - 2);
 	}
@@ -28,5 +33,15 @@ public class dp1 {
 			return qb[n];
 		else
 			return qb[n] = Fibm(n - 1, qb) + Fibm(n - 2, qb);
+	}
+	public static int Fibt(int n)
+	{	if(n == 0 || n==1)
+		return n;
+		int[] f = new int[n+1];
+		f[0] = 0;
+		f[1] = 1;
+		for(int i = 2;i<=n;i++)
+			f[i] = f[i-1]+f[i-2];
+		return f[n];
 	}
 }
