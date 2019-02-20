4d12b7b3708f4f20cfab47e4075dab5a0911aacf
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-20 23:54:57 +0530

    Tabulation approach added

--- DP/dp/src/dp/rodcutting.java
+++ DP/dp/src/dp/rodcutting.java
@@ -16,6 +16,12 @@ class rodcutting
                             cutRodmemo(arr, size, new int[size+1])); 
         end = System.currentTimeMillis();
         System.out.println("And the time taken by memoization approach is : "+(end-start)+"ms");
+
+        start = System.currentTimeMillis();
+        System.out.println("Maximum Obtainable Value (via tabulation) is "+ 
+                            cutRodTab(arr, size)); 
+        end = System.currentTimeMillis();
+        System.out.println("And the time taken by tabulation approach is : "+(end-start)+"ms");
   
     } 
     public static int cutRod(int[] prices, int n)
@@ -41,6 +47,19 @@ class rodcutting
             max_val = Integer.max(max_val,prices[i]+cutRod(prices,n-(i+1)));
         }
         return qb[n] = max_val;
+    }
+    public static int cutRodTab(int[] prices, int n)
+    {
+        if(n<=0)
+        return 0;
+        int[] val = new int[n+1];
+        for(int i = 1;i<=n;i++)
+        {   int max_val = Integer.MIN_VALUE;
+            for(int j = 0;j<i;j++)
+            max_val = Integer.max(max_val,prices[j]+val[i-(j+1)]);
+            val[i] = max_val;
+        }
+        return val[n];
     }
 
 }
