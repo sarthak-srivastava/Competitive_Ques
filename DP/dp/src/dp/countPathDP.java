bfe616cfc39ed0f0068fd06c3950d2e2b16812bd
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-21 21:06:03 +0530

    Tabulation approach added

--- DP/dp/src/dp/countPathDP.java
+++ DP/dp/src/dp/countPathDP.java
@@ -11,6 +11,11 @@ class countPathDP
         System.out.println("Total ways to move from 0 to 10 is(via memoization): " + cbpmemo(0,10,new int[11][11]));
         end = System.currentTimeMillis();
         System.out.println("Total time taken by this approach is:"+(end-start)+"ms");
+
+        start = System.currentTimeMillis();
+        System.out.println("Total ways to move from 0 to 10 is(via tabulation): " + cbptab(0,10));
+        end = System.currentTimeMillis();
+        System.out.println("Total time taken by this approach is:"+(end-start)+"ms");
     }
     public static int cbp(int s, int d)
     {   if(s>d)
@@ -44,4 +49,26 @@ class countPathDP
             }
             return qb[s][d] = cstod;
             }
+            public static int cbptab(int s, int d)
+        {   int[] qb = new int[d+1];
+            if(s>d)
+            return 0;
+            if(s==d)
+            return 1;
+            qb[d] = 1;
+            int cstod = 0;
+            for(int j = d-1;j>=s;j--){
+            for(int dice = 1;dice<=6;dice++)
+            {
+                int i = j + dice;
+                if(i<=d){
+                qb[j] +=qb[i];
+
+            }
+        }
+
+            
+            }
+            return qb[s];
+        }
 }
