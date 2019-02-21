f6784a812063724c87ec7e7cba497aa761c71442
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-21 20:43:35 +0530

    Memoization solution added

--- DP/dp/src/dp/countPathDP.java
+++ DP/dp/src/dp/countPathDP.java
@@ -3,8 +3,13 @@ class countPathDP
     public static void main(String[] args)
     {
         long start = System.currentTimeMillis();
-        System.out.println("Total ways to move from 0 to 10 is(via simple recursion: " + cbp(0,10));
+        System.out.println("Total ways to move from 0 to 10 is(via simple recursion): " + cbp(0,10));
         long end = System.currentTimeMillis();
+        System.out.println("Total time taken by this approach is:"+(end-start)+"ms");
+
+        start = System.currentTimeMillis();
+        System.out.println("Total ways to move from 0 to 10 is(via memoization): " + cbpmemo(0,10,new int[11][11]));
+        end = System.currentTimeMillis();
         System.out.println("Total time taken by this approach is:"+(end-start)+"ms");
     }
     public static int cbp(int s, int d)
@@ -22,4 +27,21 @@ class countPathDP
         }
         return cstod;
         }
+        public static int cbpmemo(int s, int d,int[][] qb)
+        {   if(s>d)
+            return 0;
+            if(s==d)
+            return 1;
+            if(qb[s][d]!=0)
+            return qb[s][d];
+    
+            int cstod = 0;
+            for(int dice = 1;dice<=6;dice++)
+            {
+                int i = s + dice;
+                int citod = cbpmemo(i,d,qb);
+                cstod += citod;
+            }
+            return qb[s][d] = cstod;
+            }
 }
