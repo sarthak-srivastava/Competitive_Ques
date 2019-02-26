81e5a54ae5abd276029d3d1a3d272463c03cf08d
Sarthak Srivastava <sarth1597@gmail.com>
2019-02-26 20:39:54 +0530

    https://www.interviewbit.com/problems/remove-punctuations/

---
+++ RemovePunctuation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Incase you have to take input, please take it from file named 'input' (without quotes) [e.g. cat input]
+# Print your final output to console. Do not redirect to another file.
+# E.g. Suppose the question is to print the content of a file.
+#      Your solution should be 'cat input'(without quotes) instead of 'cat input > output'. That's it!
+# Your code starts from here...
+functionStripAndPrint()
+{
+  cat input | tr -d '[:punct:]'
+}
+
+functionStripAndPrint cat input
