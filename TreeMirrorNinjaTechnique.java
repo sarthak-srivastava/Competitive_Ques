4c933e8edf612b4899989d0918a9c4e56530bf57
Sarthak Srivastava <sarth1597@gmail.com>
2019-03-06 14:17:04 +0530

    https://practice.geeksforgeeks.org/problems/mirror-tree/1

---
+++ TreeMirrorNinjaTechnique.java
@@ -0,0 +1,119 @@
+{
+//Initial Template for Java
+// INITIAL CODE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+// A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+class Mirror_Tree
+{
+	 void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+ 
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+ 
+        inOrder(node.right);
+    }
+	
+    // driver function to test the above functions
+    public static void main(String args[])
+    {
+        // Input the number of test cases you want to run
+        Scanner sc = new Scanner(System.in);
+		Mirror_Tree mt = new Mirror_Tree();
+        int t = sc.nextInt();
+        while (t > 0)
+        {
+            HashMap<Integer, Node> m = new HashMap<Integer, Node> ();
+            int n = sc.nextInt();
+            Node root = null;
+            while (n > 0)
+            {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                char lr = sc.next().charAt(0);
+                //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+                Node parent = m.get(n1);
+                if (parent == null)
+                {
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+                n--;
+            }
+            GfG g = new GfG();
+            g.mirror(root);
+			mt.inOrder(root);
+			System.out.println();
+            t--;
+        }
+    }
+}
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)    {
+        data = item;
+        left = right = null;
+    }
+} */
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+//function Template for Java
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)    
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+class GfG
+{
+    void mirror(Node node)
+    {
+	// Your code here
+	if(node == null)
+	return ;
+		Node t = node.left;
+	node.left = node.right;
+	node.right = t;
+	mirror(node.left);
+
+	mirror(node.right);
+	
+    }
+}
