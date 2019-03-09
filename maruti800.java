4a5464bf77f9e73f939eb67ee9abe634879beeac
Sarthak Srivastava <sarth1597@gmail.com>
2019-03-09 23:57:34 +0530

    blahblah

---
+++ maruti800.java
@@ -0,0 +1,28 @@
+class Car{
+public Car() {
+System.out.println("Class Car"); }
+public void vehicleType() {
+System.out.println("Vehicle Type:
+Car"); } }
+class Maruti extends Car{
+public Maruti() {
+System.out.println("Class Maruti"); }
+public void brand() {
+System.out.println("Brand: Maruti");}
+public void speed() {
+System.out.println("Max: 90Kmph");
+} }
+public class maruti800 extends Maruti{
+public maruti800() {
+System.out.println("Maruti Model:
+800"); }
+public void speed() {
+System.out.println("Max: 80Kmph");}
+public static void main(String args[])
+{
+maruti800 obj=new maruti800();
+obj.vehicleType();
+obj.brand();
+obj.speed();
+}
+}
