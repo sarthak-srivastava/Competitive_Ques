aa88106a5fb463c7546a304bd91d48b311f5db9e
Sarthak Srivastava <sarth1597@gmail.com>
2019-03-31 10:24:22 +0530

    https://www.interviewbit.com/problems/grid-unique-paths/

---
+++ Amazon/GridUniquePaths.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int uniquePaths(int A, int B) {
+        if(A == 1 || B == 1)
+        return 1;
+        return uniquePaths(A-1,B)+uniquePaths(A,B-1);
+        
+    }
+}
