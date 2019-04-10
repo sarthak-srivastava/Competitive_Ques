f077d8ed0e11903925dcc9c4c979aaf0649768ca
Sarthak Srivastava <sarth1597@gmail.com>
2019-04-10 14:29:26 +0530

    Domino Piling 50A

---
+++ CodeForces/DominoPiling.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class DominoPiling
+{
+    public static void main(String[] args)
+    {
+        Scanner sc  = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((m*n)/2);
+    }
+}
