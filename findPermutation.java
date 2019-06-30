6edadff80144d850191d1e156074e04e00340bd7
Sarthak Srivastava <sarth1597@gmail.com>
2019-06-30 16:51:40 +0530

    https://www.interviewbit.com/problems/find-permutation/

---
+++ findPermutation.java
@@ -0,0 +1,24 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    // static ArrayList<Integer> arr ;
+    public ArrayList<Integer> findPerm(final String A, int B) {
+    ArrayList<Integer> ans = new ArrayList<>();
+        int max = B;
+        int min = 1;
+        
+        for (char c : A.toCharArray()) {
+            if (c == 'D') {
+                ans.add(max);
+                max--;
+            }
+            else {
+                ans.add(min);
+                min++;
+            }
+        }
+        
+        ans.add(min);
+        
+        return ans;
+    }
+}
