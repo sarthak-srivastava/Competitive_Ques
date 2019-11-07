6a019a4f3a7859c2bf59fd6025d02d26839c7f07
Sarthak Srivastava <sarth1597@gmail.com>
2019-11-07 17:56:04 +0530

    https://leetcode.com/problems/boats-to-save-people

---
+++ Google/BoatToSavePeople.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int ans = 0;
+        while(i<j)
+        {   
+            if(people[i] + people[j] <= limit)
+            {
+                ans++;
+                i++;
+                j--;
+            }
+            else
+            {
+                j--;
+                ans++;
+            }
+        }
+        
+        if(i == j)
+            ans++;
+        
+        return ans;
+    }
+}
