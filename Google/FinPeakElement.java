bf9d8430aa24cb35f3dc61d6828e1aac3cdcd598
Sarthak Srivastava <sarth1597@gmail.com>
2019-11-07 13:28:21 +0530

    https://leetcode.com/problems/find-peak-element/

---
+++ Google/FinPeakElement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 1 )
+            return 0;
+        int left = 0 ;
+        int right  = nums.length - 1;
+        while(left<right)
+        {
+            int mid = left + (right - left)/2;
+            if(nums[mid]<nums[mid+1])
+                left = mid+1;
+            else
+                right = mid;
+        }
+        return left;
+        
+    }
+}
