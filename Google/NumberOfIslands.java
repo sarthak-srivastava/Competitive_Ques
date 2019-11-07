8143e2ecf4fb7101704279d302c08a60abf72949
Sarthak Srivastava <sarth1597@gmail.com>
2019-11-07 17:27:00 +0530

    https://leetcode.com/problems/number-of-islands

---
+++ Google/NumberOfIslands.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+      if(grid == null || grid.length == 0)
+        return 0;
+        int ans = 0;
+      for(int i = 0; i<grid.length;i++)
+      {
+          for(int j = 0; j<grid[0].length;j++)
+          {
+              if(grid[i][j] == '1')
+              {
+                  dfs(grid,i,j);
+                  ans++;
+              }
+          }
+      }
+        return ans;
+    }
+    public static void dfs(char[][] grid, int i, int j)
+    {
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j] == '0')
+        {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+        return;
+    }
+}
