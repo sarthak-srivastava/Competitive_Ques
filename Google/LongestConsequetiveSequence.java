2157ae7e2c6351d2dba31ab17de8aa6429060395
Sarthak Srivastava <sarth1597@gmail.com>
2019-11-08 09:18:34 +0530

    https://leetcode.com/problems/longest-consecutive-sequence

---
+++ Google/LongestConsequetiveSequence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        if(nums.length == 1)
+            return 1;
+        HashSet<Integer> hs = new HashSet<Integer>();
+        int count = 1;
+        int ans = 1;
+        for(int i:nums)
+        {
+            if(!hs.contains(i))
+                hs.add(i);
+
+            }
+        for(int i:nums)
+        {   count = 1;
+            if(hs.contains(i+1))
+            {   int j = i;
+                while(hs.contains(j+1))
+                {
+                    count++;
+                    j = j + 1;
+                }
+             ans = Integer.max(count,ans);
+            }
+        }
+         ans = Integer.max(count,ans);
+        return ans;
+    }
+}
