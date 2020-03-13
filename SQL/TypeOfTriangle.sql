21e1ce5b60a4a8d56aea8c01eed37cb126f1961e
Sarthak Srivastava <sarth1597@gmail.com>
2020-03-13 16:00:26 +0530

    https://www.hackerrank.com/challenges/what-type-of-triangle/problem

---
+++ SQL/TypeOfTriangle.sql
@@ -0,0 +1,13 @@
+/*
+Enter your query here.
+*/
+SELECT (CASE
+        WHEN (A+B<=C OR B+C<=A OR C+A<=B)
+        THEN "Not A Triangle"
+        WHEN (A=B AND B=C AND C=A)
+        THEN "Equilateral"
+        WHEN (A=B OR B=C OR C=A)
+        THEN "Isosceles"
+        ELSE "Scalene"
+        END)
+FROM TRIANGLES;
