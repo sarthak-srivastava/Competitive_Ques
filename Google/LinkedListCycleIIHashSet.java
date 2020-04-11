77f9ef03d79a1e8e36807b65834a5dc699bc4a87
Sarthak Srivastava <sarth1597@gmail.com>
2020-04-12 03:55:03 +0530

    https://leetcode.com/problems/linked-list-cycle-ii/

---
+++ Google/LinkedListCycleIIHashSet.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null)
+            return null;
+        HashSet<ListNode> hs = new HashSet<>();
+        while(head!=null)
+        {
+            if(hs.contains(head))
+                return head;
+            hs.add(head);
+            head = head.next;
+        }
+        
+        return null;
+    }
+}
