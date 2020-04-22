6289cb4748f96b71d16229453e5ad1d87dcbf2b6
Sarthak Srivastava <sarth1597@gmail.com>
2020-04-22 06:45:45 +0530

    https://leetcode.com/problems/distinct-subsequences

---
+++ Facebook/DistinctSubsequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+       if(t == null || t.length() == 0)
+           return s.length();
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for(int i = 0; i<=s.length(); i++)
+            dp[i][0] = 1;
+        for(int i = 1; i<=s.length(); i++)
+        {
+            for(int j = 1; j<=t.length(); j++)
+            {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] += dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] += dp[i-1][j];
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+}
