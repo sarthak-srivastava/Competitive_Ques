72d4923dabe73461ae8b1b4c1b0efbc5fdcb6e17
Sarthak Srivastava <sarth1597@gmail.com>
2020-05-08 20:12:15 +0530

    https://www.lintcode.com/problem/add-and-search-word-data-structure-design

---
+++ Facebook/AddSearchWordTrie.java
@@ -0,0 +1,44 @@
+public class WordDictionary {
+    /*
+     * @param word: Adds a word into the data structure.
+     * @return: nothing
+     */
+    public class TrieNode {
+        public TrieNode[] children = new TrieNode[26];
+        public boolean isEnd = false;
+    }
+    
+    private TrieNode root = new TrieNode();
+
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return match(word.toCharArray(), 0, root);
+    }
+    
+    private boolean match(char[] chs, int k, TrieNode node) {
+        if (k == chs.length) return node.isEnd;   
+        if (chs[k] != '.') {
+            return node.children[chs[k] - 'a'] != null && match(chs, k + 1, node.children[chs[k] - 'a']);
+        } else {
+            for (int i = 0; i < node.children.length; i++) {
+                if (node.children[i] != null) {
+                    if (match(chs, k + 1, node.children[i])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    
+    }
+}
