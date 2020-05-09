06c2f1f861216e3c2f4db8156137168ce18e5a1c
Sarthak Srivastava <sarth1597@gmail.com>
2020-05-09 13:11:26 +0530

    https://www.lintcode.com/problem/implement-strstr

---
+++ Facebook/strSTR.java
@@ -0,0 +1,51 @@
+public class Solution {
+    /**
+     * @param source: 
+     * @param target: 
+     * @return: return the index
+     */
+    public int strStr(String haystack, String needle) {
+        // Write your code here
+        if(needle.length() == 0 && haystack.length() != 0)
+            return 0;
+            
+        if(haystack.length() == 0 && needle.length() != 0)
+                return -1;
+        if(haystack.length()<needle.length())
+            return -1;
+        int len = needle.length();
+        StringBuilder str = new StringBuilder("");
+        int i = 0;
+        
+        for(i = 0; i<len; i++)
+        {
+            str.append(haystack.charAt(i));
+        }
+        StringBuilder target = new StringBuilder(needle);
+        if(Equals(str,target) == true)
+            return 0;
+        int c = 1;
+        while(i<haystack.length())
+        {
+            str.deleteCharAt(0);
+            str.append(haystack.charAt(i));
+            boolean eq = Equals(str,target);
+            if(eq == true)
+                return c;
+            c++;
+            i++;
+        }
+        return -1;
+    }
+    boolean Equals(StringBuilder str,StringBuilder target)
+    {
+        if(str.length()!=target.length())
+            return false;
+        for(int i = 0; i<str.length();i++)
+        {
+            if(str.charAt(i)!=target.charAt(i))
+                return false;
+        }
+        return true;
+    }
+}
