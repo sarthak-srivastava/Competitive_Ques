f7dd13fbdc98a7730b5ed3250bbfe08fec82fdbb
Sarthak Srivastava <sarth1597@gmail.com>
2020-05-11 20:16:20 +0530

    Addition of HashMap to avoid redundant BST sum calculations

--- Facebook/MaxSumBSTinBT.java
+++ Facebook/MaxSumBSTinBT.java
@@ -24,8 +24,20 @@
 class Solution {
     int ans = 0;
     int sum = 0;
+    class sumBST
+    {
+        boolean bst;
+        int sum;
+        sumBST(boolean a, int b)
+        {
+            bst = a;
+            sum = b;
+        }
+    }
+    HashMap<TreeNode, sumBST> hm;
     public int maxSumBST(TreeNode root) {
         ans = 0;
+        hm = new HashMap<>();
         inOrderTraversal(root);
         return ans;
     }
@@ -35,15 +47,33 @@ class Solution {
             return;
         inOrderTraversal(root.left);
         sum = 0;
-        if(isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE))
+        if(hm.containsKey(root))
+        {
+            sumBST tmp = hm.get(root);
+            if(tmp.bst)
+                ans = Integer.max(ans, tmp.sum);
+        }
+        else{
+        boolean t = isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        if(t)
         ans = Integer.max(ans, sum);
+        sumBST tmp = new sumBST(t, sum);
+        hm.put(root, tmp);}
         inOrderTraversal(root.right);
     }
     public boolean isBST(TreeNode root, int min_lim, int max_lim)
     {   if(root == null)
             return true;
+        
         if(root.val<= min_lim || root.val>= max_lim)
             return false;
+        if(hm.containsKey(root))
+        {
+            sumBST tmp = hm.get(root);
+                sum += tmp.sum;
+                return tmp.bst;
+        
+        }
         sum += root.val;
         return isBST(root.left, min_lim, root.val) && isBST(root.right, root.val, max_lim);
     }
